672b78484452b9bd34001c71a4edeb84154adb88
Dhanvantri Vishwakarma (DV) <121880086+DvKrishaaSoft@users.noreply.github.com>
2026-07-19 20:50:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,22 @@ Visit Website →
 `;
 
 });
+
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+
+anchor.addEventListener('click', function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+});
+
+});
+
+
