0b4d6fa749e3893393793643c358de7196961460
Dhanvantri Vishwakarma (DV) <121880086+DvKrishaaSoft@users.noreply.github.com>
2026-07-19 19:24:31 +0530

    Change header background on scroll event

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+window.addEventListener("scroll", function(){
+
+    const header = document.querySelector(".header");
+
+    if(window.scrollY > 100){
+
+        header.style.background = "#020617";
+
+    }else{
+
+        header.style.background = "#111827";
+
+    }
+
+});
