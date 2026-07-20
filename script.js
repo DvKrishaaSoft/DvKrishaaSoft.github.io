59d1561a53651f34241e7800805956c14c7759ea
Dhanvantri Vishwakarma (DV) <121880086+DvKrishaaSoft@users.noreply.github.com>
2026-07-20 14:31:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,85 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+// ddddd
+const projectsContainer = document.getElementById("projects");
+const searchInput = document.getElementById("search");
+const categorySelect = document.getElementById("category");
+
+let allProjects = [];
+
+async function loadProjects() {
+    try {
+        const response = await fetch("projects.json");
+        allProjects = await response.json();
+
+        displayProjects(allProjects);
+    } catch (error) {
+        console.error("Error loading projects:", error);
+    }
+}
+
+function displayProjects(projects) {
+
+    projectsContainer.innerHTML = "";
+
+    if (projects.length === 0) {
+        projectsContainer.innerHTML = "<h3>No Projects Found</h3>";
+        return;
+    }
+
+    projects.forEach(project => {
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <h3>${project.name}</h3>
+
+            <p><strong>Category:</strong> ${project.category}</p>
+
+            <p><strong>Technology:</strong> ${project.technology}</p>
+
+            <a href="${project.url}" target="_blank">
+                Visit Website →
+            </a>
+        `;
+
+        projectsContainer.appendChild(card);
+
+    });
+
+}
+
+function filterProjects() {
+
+    const searchText = searchInput.value.toLowerCase();
+
+    const selectedCategory = categorySelect.value;
+
+    const filtered = allProjects.filter(project => {
+
+        const matchesSearch =
+            project.name.toLowerCase().includes(searchText) ||
+            project.technology.toLowerCase().includes(searchText);
+
+        const matchesCategory =
+            selectedCategory === "All" ||
+            project.category === selectedCategory;
+
+        return matchesSearch && matchesCategory;
+
+    });
+
+    displayProjects(filtered);
+
+}
+
+searchInput.addEventListener("input", filterProjects);
+
+categorySelect.addEventListener("change", filterProjects);
+
+loadProjects();
+
+
